e30dcac5af032070c06bcda6fd76f577d7e27e3a
yottanote <information@yottanote.com>
2017-06-15 12:00:52 +0900

    2nd commit

---
+++ src/index.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * yottanote.com CMS
+ *
+ * commonのindex
+ * <br>
+ * 各ドメイン共通設定
+ *
+ * @author yottanote.com
+ * @since 2014/11/11
+ * @version 1.2
+ * @date 2014/11/11
+ *
+ * copyright yottanote.com
+ */
+
+/**
+* 各種phpファイルを読み込む
+*　
+ * @since 2014/11/11
+ * @version 1.2
+*/
+include("setting.php");
+include("langauges.php");
+include("outside.php");
+include("function.php");
+include("variable_setting.php");
+include("include.php");
+include("menubar-navi.php");
+include("contents-navi.php");
+include("info.php");
+//include("counter.php");
+include("sql.php");
+include("responsive-web.php");
+
+?>
